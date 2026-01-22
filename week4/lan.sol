d52df470d8ed7e660c9ff3eedd57d21006c2b3c1
seacrab808 <seacrab808@naver.com>
2026-01-22 10:18:50 +0900

    '솔리디티 문법'

---
+++ week4/lan.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+// contract Ex4_1 {
+//     uint public a = 3;
+//     function myFun() public {
+//         a = 5;
+//     }
+// }
+
+// contract Ex4_2 {
+//     uint public a = 3;
+//     function myFun(uint b, uint c, uint d) public {
+//         a = b;
+//         a = c;
+//         a = d;
+//     }
+// }
+
+// contract Ex4_3 {
+//     uint public a = 3;
+//     uint public b = 5;
+//     function myFun() public returns(uint, uint) {
+//         a = 100;
+//         b = 0;
+//         return (a, b);
+//     }
+// }
+
+// contract Ex4_4 {
+//     function myFun() public pure returns(uint age, uint weight) {
+//         age = 31;
+//         weight = 60;
+//     }
+// }
+

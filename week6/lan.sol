4767924937a4f50a1bb94d99a333325aad40c003
seacrab808 <seacrab808@naver.com>
2026-01-26 16:30:33 +0900

    "week 6"

--- week6/lan.sol
+++ week6/lan.sol
@@ -38,22 +38,56 @@ pragma solidity >=0.7.0 < 0.9.0;
 //     }
 // }
 
-contract Number {
-    uint private num = 4;
-    function changeNum() public {
-        num = 5;
-    }
-    function getNum() public view returns(uint) {
-        return num;
+// contract Number {
+//     uint private num = 4;
+//     function changeNum() public {
+//         num = 5;
+//     }
+//     function getNum() public view returns(uint) {
+//         return num;
+//     }
+// }
+
+// contract Caller {
+//     Number internal instance = new Number();
+//     function changeNumber() public {
+//         instance.changeNum();
+//     }
+//     function getNumber() public view returns(uint) {
+//         return instance.getNum();
+//     }
+// }
+
+// contract Student {
+//     string public schoolName = "The University of Solidity";
+// }
+
+// contract ArtStudent is Student {
+//     function changeSchoolName() public {
+//         schoolName = "The University of Blockchain";
+//     }
+//     function getSchoolName() public view returns(string memory) {
+//         return schoolName;
+//     }
+// }
+
+contract Student {
+    string public schoolName = "The University of Solidity";
+    string public major;
+    constructor(string memory _major) {
+        major = _major;
     }
 }
 
-contract Caller {
-    Number internal instance = new Number();
-    function changeNumber() public {
-        instance.changeNum();
+contract ArtStudent is Student("Art") {
+}
+
+contract MusicStudent is Student {
+    constructor() Student("Music"){
     }
-    function getNumber() public view returns(uint) {
-        return instance.getNum();
+}
+
+contract MathStudent is Student {
+    constructor(string memory _major) Student(_major) {
     }
 }
